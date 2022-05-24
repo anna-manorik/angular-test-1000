7b53bee443f80a0be5cc568f9e2e9e349e05277e
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-24 23:12:11 +0300

    Create header.component.ts

---
+++ exchange/src/app/header/header.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class Header implements OnInit {
+  response: any;
+  rateUSD: any;
+  rateEUR: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+
+    let myHeaders: HttpHeaders = new HttpHeaders();
+    myHeaders = myHeaders.append('Content-Type', 'application/json');
+    myHeaders = myHeaders.append("apikey", "85G3Ou0cnHfZmh5zO86CQY5dGN8nphcc");
+    
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: myHeaders
+    };
+
+    this.http
+      .get('https://api.apilayer.com/fixer/latest?symbols=UAH&base=USD', requestOptions)
+      .subscribe((response) => {
+        this.rateUSD = response;
+      });
+
+    this.http
+      .get('https://api.apilayer.com/fixer/latest?symbols=UAH&base=EUR', requestOptions)
+      .subscribe((response) => {
+        this.rateEUR = response;
+      });
+  }
+
+}
