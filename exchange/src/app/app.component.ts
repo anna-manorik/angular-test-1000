682d94d00d67906fbe61c80643711460dd944143
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-24 23:49:33 +0300

    Update app.component.ts

--- exchange/src/app/app.component.ts
+++ exchange/src/app/app.component.ts
@@ -7,29 +7,29 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'todo';
+  // title = 'todo';
 
-  filter: 'all' | 'active' | 'done' = 'all';
+  // filter: 'all' | 'active' | 'done' = 'all';
 
-  allItems = [
-    { description: 'eat', done: true },
-    { description: 'sleep', done: false },
-    { description: 'play', done: false },
-    { description: 'laugh', done: false },
-  ];
+  // allItems = [
+  //   { description: 'eat', done: true },
+  //   { description: 'sleep', done: false },
+  //   { description: 'play', done: false },
+  //   { description: 'laugh', done: false },
+  // ];
 
-  get items() {
-    if (this.filter === 'all') {
-      return this.allItems;
-    }
-    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
-  }
+  // get items() {
+  //   if (this.filter === 'all') {
+  //     return this.allItems;
+  //   }
+  //   return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  // }
 
-  addItem(description: string) {
-    this.allItems.unshift({
-      description,
-      done: false
-    });
-  }
+  // addItem(description: string) {
+  //   this.allItems.unshift({
+  //     description,
+  //     done: false
+  //   });
+  // }
 
 }
