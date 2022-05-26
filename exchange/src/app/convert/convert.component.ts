6efb3c6f451906f71595772ab2ca9633b5109c0f
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-26 12:41:31 +0300

    Update convert.component.ts

--- exchange/src/app/convert/convert.component.ts
+++ exchange/src/app/convert/convert.component.ts
@@ -1,42 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-convert',
   templateUrl: './convert.component.html',
   styleUrls: ['./convert.component.css'],
 })
-
-export class Convert implements OnInit {
-  response: any;
-  rateUSD: any;
-  rateEUR: any;
+export class Convert {
+  fromValue: any = '1';
+  toValue: any;
+  fromCurr: any = 'USD';
+  toCurr: any = 'UAH';
+  resultRate: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-
-    let myHeaders: HttpHeaders = new HttpHeaders();
-    myHeaders = myHeaders.append('Content-Type', 'application/json');
-    myHeaders = myHeaders.append("apikey", "85G3Ou0cnHfZmh5zO86CQY5dGN8nphcc");
-    
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-      headers: myHeaders
-    };
-
-    this.http
-      .get('https://api.apilayer.com/fixer/latest?symbols=UAH&base=USD', requestOptions)
-      .subscribe((response) => {
-        this.rateUSD = response;
-      });
-
-    this.http
-      .get('https://api.apilayer.com/fixer/latest?symbols=UAH&base=EUR', requestOptions)
-      .subscribe((response) => {
-        this.rateEUR = response;
-      });
+    this.getCurrencyRateOne();
   }
 
+  async getCurrencyRateOne() {
+    this.http.get(`http://free.currencyconverterapi.com/api/v5/convert?q=${this.fromCurr}_${this.toCurr}&compact=y&apiKey=83c8b3ef61f64ee9a07ed25fc8de3a54`)
+    .subscribe((response) => {
+      this.resultRate = Object.values(response)[0].val;
+        this.calculateCurrencyOne();
+    });
+  }
+
+  async getCurrencyRateTwo() {
+    this.http.get(`http://free.currencyconverterapi.com/api/v5/convert?q=${this.fromCurr}_${this.toCurr}&compact=y&apiKey=83c8b3ef61f64ee9a07ed25fc8de3a54`)
+    .subscribe((response) => {
+      this.resultRate = Object.values(response)[0].val;
+        this.calculateCurrencyTwo();
+    });
+  }
+
+  calculateCurrencyOne() {
+    if(this.fromValue){
+      this.toValue = (this.fromValue * parseFloat(this.resultRate)).toFixed(1);
+    }
+  }
+
+  calculateCurrencyTwo() {
+    this.fromValue = (this.toValue / parseFloat(this.resultRate)).toFixed(1);
+  }
 }
