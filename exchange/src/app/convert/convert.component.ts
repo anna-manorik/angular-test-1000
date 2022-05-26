3dd9f2295343e5bf9b766f137d5082b7c6c76987
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-26 15:30:15 +0300

    Update convert.component.ts

--- exchange/src/app/convert/convert.component.ts
+++ exchange/src/app/convert/convert.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-convert',
   templateUrl: './convert.component.html',
