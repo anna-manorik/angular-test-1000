63c28753b0c7641e8b3f28b32499333cadd138e8
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-05-26 12:41:24 +0300

    Update app.module.ts

--- exchange/src/app/app.module.ts
+++ exchange/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Header } from './header/header.component';
-
+import { Convert } from './convert/convert.component';
 
 @NgModule({
-  declarations: [AppComponent, Header],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, Header, Convert],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
